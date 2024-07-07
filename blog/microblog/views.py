db778fa592d59a97b5e633a47c1eb8ff9567ba7d
Joa98Dev <joaquinpuente98@gmail.com>
2024-07-06 18:35:55 -0600

    Create posts feature added, index page has been improved and other major updates

--- blog/microblog/views.py
+++ blog/microblog/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView
+from .models import BlogPost
 
 # Create your views here.
-class Index(View):
+class Index(ListView):
+    model = BlogPost
+    queryset = BlogPost.objects.all().order_by('-date')
+    template_name = 'microblog/index.html'
+
+
+    '''
     def get(self, request):
         return render(request, 'microblog/index.html')
+    '''
     
-    '''https://youtu.be/sMqDJovFO-Y?si=2ulxWmYxLLSz2El2&t=781'''
+    #https://youtu.be/sMqDJovFO-Y?si=2ulxWmYxLLSz2El2&t=781
