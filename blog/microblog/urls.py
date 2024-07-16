4572295495b640b70485671b706dc7a1b15c8e91
Joa98Dev <joaquinpuente98@gmail.com>
2024-07-15 23:23:12 -0600

    Like/unlike button woking, the must log to like or unlike the post is working, bugs already fixed and some major updates to the microblog-views.py code

--- blog/microblog/urls.py
+++ blog/microblog/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('tinymce/', include('tinymce.urls')),
     path('', Index.as_view(), name='index'),
     path('<int:pk>/', DetailPostView.as_view(), name='detail_post'),
-    path('<int:pk>/like', LikePost.as_view(), name='like_post'),
+    path('<int:pk>/like/', LikePost.as_view(), name='like_post'),
 ]
--- blog/microblog/views.py
+++ blog/microblog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import ListView, DetailView
 from .models import BlogPost
@@ -11,26 +11,44 @@ class Index(ListView):
     paginate_by = 1
 
 
+
 class DetailPostView(DetailView):
     model = BlogPost
     template_name = 'microblog/post.html'
 
-    def get_context_data(self, *args, **kwargs):
-        context = super(DetailPostView, self).get_context_data(*args, **kwargs)
+    def get_context_data(self, **kwargs):
+        context = super(DetailPostView, self).get_context_data(**kwargs)
+        context['liked_by_user'] = False
+        micropost = self.get_object()
+        if self.request.user.is_authenticated and micropost.likes.filter(pk=self.request.user.id).exists():
+            context['liked_by_user'] = True
+        context['object'] = micropost  # Ensure object is in context
+        return context
+
+'''
+
+class DetailPostView(DetailView):
+    model = BlogPost
+    template_name = 'microblog/post.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(DetailPostView, self).get_context_data(**kwargs)
         context['liked_by_user'] = False
         micropost = BlogPost.objects.get(id=self.kwargs.get('pk'))
         if micropost.likes.filter(pk=self.request.user.id).exists():
-            return True
+            context['liked_by_user'] = True
         else:
-            return context
-        
+
+           return context
+'''        
 class LikePost(View):
     def post(self, request, pk):
-        micropost = Post.objects.get(id=pk)
+        print("LikePost called with pk={pk} by user={request.user}")
+        micropost = BlogPost.objects.get(id=pk)
         if micropost.likes.filter(pk=self.request.user.id).exists():
             micropost.likes.remove(request.user.id)
         else:
             micropost.likes.add(request.user.id)
 
         micropost.save()
-        return redirect('detail_post',pk)
+        return redirect('detail_post',pk=pk)
